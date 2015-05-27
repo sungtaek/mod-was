764f910fab9c6a9d0bd60b8ac0ae02e1c6187cf0
sungtaek <leesungtaek@gmail.com>
2015-05-27 22:47:44 +0900

    change AppVerticle

--- src/main/java/org/lima/vertx/mod_was/AppVerticle.java
+++ src/main/java/org/lima/vertx/mod_was/AppVerticle.java
@@ -17,84 +17,17 @@ package org.lima.vertx.mod_was;
  * @author <a href="http://tfox.org">Tim Fox</a>
  */
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.lima.vertx.mod_was.handler.EBDataHandler;
-import org.lima.vertx.mod_was.model.AppInfo;
-import org.lima.vertx.mod_was.model.UrlMapper;
-import org.lima.vertx.mod_was.server.AppServer;
 import org.vertx.java.busmods.BusModBase;
-import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.core.logging.Logger;
 import org.vertx.java.core.logging.impl.LoggerFactory;
 
 public class AppVerticle extends BusModBase {
 	private Logger logger = LoggerFactory.getLogger(AppVerticle.class);
-	private Map<String, AppInfo> appInfoMap = new HashMap<String, AppInfo>();
-	private JsonObject serverConfig = null;
-	private AppServer server = null;
 
 	public void start() {
 		super.start();
 
-		String host = null;
-		int port = 0;
-
-		logger.info("starting ServerVerticle...");
+		logger.info("starting Application Verticle...");
 		
-		serverConfig = config.getObject("server");
-		if(serverConfig == null) {
-			logger.error("not found server config");
-			vertx.stop();
-			return;
-		}
-	
-		eb.registerHandler(serverConfig.getString("channel"), new EBDataHandler<JsonObject>() {
-			@Override
-			public void handleData(JsonObject data) {
-				if("register".equalsIgnoreCase(data.getString("method"))) {
-					AppInfo appInfo = AppInfo.create(data.getObject("appInfo"));
-					registerApplication(appInfo);
-					
-					JsonObject reply = new JsonObject();
-					reply.putString("method", "register");
-					reply.putString("status", "ok");
-					
-					sendReply(reply);
-				}
-			}
-		});
-
-		
-		host = serverConfig.getString("host");
-		port = serverConfig.getInteger("port");
-
-		server = new AppServer(vertx, host, port);
-		server.init();
-		server.start();
-	}
-
-	private void registerApplication(AppInfo appInfo) {
-		if(appInfoMap.get(appInfo.getName()) == null) {
-			logger.info("register Application[" + appInfo.getName() + "]");
-			appInfoMap.put(appInfo.getName(), appInfo);
-
-			if(appInfo.getUrlMappers().size() > 0) {
-				server.stop();
-				for(UrlMapper urlMapper: appInfo.getUrlMappers()) {
-					try {
-						server.addMapper(urlMapper);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-				server.init();
-				server.start();
-			}
-		}
-		else {
-			logger.debug("already registered Application[" + appInfo.getName() + "]");
-		}
 	}
 }
